ab17ef26c63b5b4862eea06ad28fab5cc0d949ad
Mariamma Antony <Mariamma.Antony@aexp.com>
2017-10-13 19:40:07 +0530

    feed test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,45 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('has a url defined and the url is not empty', function(){
+            allFeeds.forEach(function(item){
+                expect(item.url).toBeDefined();
+                expect(item.url.length).not.toEqual(0);
+            });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name defined and the name is not empty', function(){
+            allFeeds.forEach(function(item){
+                expect(item.name).toBeDefined();
+                expect(item.name.length).not.toEqual(0);
+            });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu', function() {
+
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('element is hidden by default', function(){
+            
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+    });      
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
